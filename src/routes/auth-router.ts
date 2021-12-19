46be59c77bd738f10dad30d97c4187145d39bee2
kwon, minho <minhob38@gmail.com>
2021-12-19 21:13:49 +0900

    install swagger

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -31,4 +31,30 @@ router.post('/signup', async (req: express.Request, res: express.Response, next:
   return res.status(200).json(data);
 });
 
+router.post('/signin', async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const { email, password } = req.body as { email: string, password: string };
+  const user = await findUserByEmail(email);
+
+  if (user) {
+    const data: IResData = {
+      status: 'error',
+      message: 'user already exists',
+    };
+
+    return res.status(200).json(data);
+  }
+
+  const hash: string = createHash(password);
+  const insertedUser = await createUser(email, hash);
+  const token: string = createToken(insertedUser.email);
+
+  const data: IResData = {
+    status: 'success',
+    message: 'user signed up',
+    data: { access_token: token },
+  };
+
+  return res.status(200).json(data);
+});
+
 export default router;
