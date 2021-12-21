5357c55a3b363e8cb0a3e1134493a88c778cc32d
kwon, minho <minhob38@gmail.com>
2021-12-21 15:24:56 +0900

    set test router for test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
-import express from 'express';
-import authRouter from './auth-router';
-import boardRouter from './board-router';
+import express from "express";
+import authRouter from "./auth-router";
+import boardRouter from "./board-router";
+import testRouter from "./test-router";
 
 const router: express.Router = express.Router();
 
-router.use('/auth', authRouter);
-router.use('/board', boardRouter);
+router.use("/auth", authRouter);
+router.use("/board", boardRouter);
+router.use("/test", testRouter);
 
 export default router;
