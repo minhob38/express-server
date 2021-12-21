07028f0d88941b9ed01f65fb9450942e5dcf3538
kwon, minho <minhob38@gmail.com>
2021-12-21 21:40:58 +0900

    add prettier

--- src/routes/test-router.ts
+++ src/routes/test-router.ts
@@ -1,15 +1,22 @@
-import express from "express";
-import { IResData } from "../types";
+import express from 'express';
+import { IResData } from '../types';
 
 const router: express.Router = express.Router();
 
-router.get("/", async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const data: IResData = {
-    status: "success",
-    message: "test conducted",
-  };
+router.get(
+  '/',
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    const data: IResData = {
+      status: 'success',
+      message: 'test conducted',
+    };
 
-  return res.status(200).json(data);
-});
+    return res.status(200).json(data);
+  }
+);
 
 export default router;
