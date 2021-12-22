993547efd515422104153667ab997951ac6461a4
kwon, minho <minhob38@gmail.com>
2021-12-23 00:24:42 +0900

    add bearer auth to swagger

--- src/config/swagger-config.ts
+++ src/config/swagger-config.ts
@@ -2,6 +2,7 @@ import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
 // open api document - https://swagger.io/specification/
+// https://www.npmjs.com/package/express-jsdoc-swagger
 const options = {
   swaggerDefinition: {
     openapi: '3.0.0',
@@ -11,11 +12,16 @@ const options = {
       description: 'api server specification',
     },
     host: 'localhost:8000',
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+        },
+      },
+    },
   },
-  apis: [
-    'src/routes/auth-router.ts',
-    'src/routes/board-router.ts'
-  ],
+  apis: ['src/routes/auth-router.ts', 'src/routes/board-router.ts'],
 };
 
 const openapiSpecification = swaggerJsdoc(options);
