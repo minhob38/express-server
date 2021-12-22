c31673c30c2fcf6c44a0a5d758f469f4cb34f727
kwon, minho <minhob38@gmail.com>
2021-12-23 00:25:33 +0900

    add userinfo to express request type

---
+++ src/types/types.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+
+export interface IResData {
+  status: string;
+  message: string;
+  data?: any;
+}
+
+export interface IUser {
+  userId: string;
+  email: string;
+  password: string;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface IPost {
+  postId: string;
+  author: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+// export interface IRequestUserInfo extends express.Request {
+//   userInfo: { email: string };
+// }
+
+export interface IUserInfo {
+  email: string;
+}
+
+export interface IJwtPayloadUserInfo extends jwt.JwtPayload {
+  email: string;
+}
