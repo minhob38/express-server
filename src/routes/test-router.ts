749890ac0103ba578fc083bb4870c804bc0b5d3d
kwon, minho <minhob38@gmail.com>
2022-01-07 00:57:12 +0900

    test commit

--- src/routes/test-router.ts
+++ src/routes/test-router.ts
@@ -15,7 +15,7 @@ router.get(
   ) => {
     const data: IResData = {
       status: 'success',
-      message: 'test conducted :3',
+      message: 'test conducted',
     };
 
     return res.status(200).json(data);
