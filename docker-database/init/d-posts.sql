fc71cbe2de3b93b9d87843a98a768e8cc406fb9c
kwon, minho <minhob38@gmail.com>
2022-01-20 22:22:11 +0900

    add shp data

--- docker-database/init/d-posts.sql
+++ docker-database/init/d-posts.sql
@@ -16,8 +16,6 @@ VALUES
 (2, 'abcde@gmail.com', 'django server', 'django server ...', '2021-12-22 12:46:31.150', NULL),
 (3, 'abcde@gmail.com', 'koa server', 'koa server ...', '2021-12-23 12:46:31.150', NULL),
 (4, 'qwert@gmail.com', 'flask server', 'flask server ...', '2021-12-24 12:46:31.150', NULL),
-(5, 'qwert@gmail.com', 'spring server', 'spring server ...', '2021-12-25 12:46:31.150', NULL);
-(6, 'zxcvb@gmail.com', 'php server', 'php server ...', '2021-12-26 12:46:31.150', NULL);
+(5, 'qwert@gmail.com', 'spring server', 'spring server ...', '2021-12-25 12:46:31.150', NULL),
+(6, 'zxcvb@gmail.com', 'php server', 'php server ...', '2021-12-26 12:46:31.150', NULL),
 (7, 'zxcvb@gmail.com', 'go server', 'go server ...', '2021-12-27 12:46:31.150', NULL);
-
-
