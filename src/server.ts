bf343c5aaac7f1a2e172b1adf2e3036de551742d
kwon, minho <minhob38@gmail.com>
2022-02-15 13:10:11 +0900

    implement cross-env

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import app from './app';
 
 const server: http.Server = http.createServer(app);
 const port: number = 8000;
-
+console.log(process.env.NODE_ENV);
 server.listen(port, () => console.log(`server connection: port ${port}`));
 
 export default server;
--- test/unit/utils/auth-util.test.ts
+++ test/unit/utils/auth-util.test.ts
@@ -1,5 +1,6 @@
 import { createToken } from '@src/utils/auth-util';
 
+console.log(process.env.NODE_ENV);
 describe('createToken 함수는 json web token을 반환해야합니다.', () => {
   test('token은 문자열입니다.', () => {
     const token: string = createToken('minhob38@gmail.com');
