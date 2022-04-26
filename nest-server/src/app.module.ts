21bc3c27c9144672e9b76c834b90eddba4600794
kwon, minho <minhob38@gmail.com>
2022-04-27 00:18:51 +0900

    create auths module

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthsModule } from './auths/auths.module';
 
 @Module({
-  imports: [],
+  imports: [AuthsModule],
   controllers: [AppController],
   providers: [AppService],
 })
