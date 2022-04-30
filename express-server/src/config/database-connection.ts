ca86f2c39d6c5f9676095b1420be9163acdb850a
kwon, minho <minhob38@gmail.com>
2022-04-30 15:58:23 +0900

    baseline commit

--- express-server/src/config/database-connection.ts
+++ express-server/src/config/database-connection.ts
@@ -2,7 +2,7 @@ import { Knex, knex } from 'knex';
 import dotenv from 'dotenv';
 import { INVALID_ENVIROMENT_VARIABLE } from '../constants/error';
 
-dotenv.config()
+dotenv.config();
 
 const {
   DOCKER_DB_NAME,
@@ -12,11 +12,12 @@ const {
   DOCKER_DB_PASSWORD,
 } = process.env;
 
-if (!DOCKER_DB_NAME
-  || !DOCKER_DB_USER
-  || !DOCKER_DB_HOST
-  || !DOCKER_DB_PORT
-  || !DOCKER_DB_PASSWORD
+if (
+  !DOCKER_DB_NAME ||
+  !DOCKER_DB_USER ||
+  !DOCKER_DB_HOST ||
+  !DOCKER_DB_PORT ||
+  !DOCKER_DB_PASSWORD
 ) {
   throw new Error(INVALID_ENVIROMENT_VARIABLE);
 }
