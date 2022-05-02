9b65128e21695f313bcbfcdc15e9fe26de3f7e44
kwon, minho <minhob38@gmail.com>
2022-05-02 22:18:47 +0900

    set typeorm

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthsModule } from './auths/auths.module';
 
+// forRoot(동적모듈?)
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: parseInt(process.env.DOCKER_DB_PORT, 10),
+      username: process.env.DOCKER_DB_USER,
+      password: process.env.DOCKER_DB_PASSWORD,
+      database: process.env.DOCKER_DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
     }),
     AuthsModule,
   ],
