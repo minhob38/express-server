4ee065a6b5869623eb38524499d826b94032cb30
kwon, minho <minhob38@gmail.com>
2022-05-13 22:11:27 +0900

    implement joi to validate env

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -5,12 +5,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthsModule } from './auths/auths.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import validationSchema from './configs/validationSchema';
 
 // forRoot(동적모듈?)
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
+      // load: [databaseConfig, smsConfig, googleOAuthConfig, jwtConfig],
+      isGlobal: true,
+      validationSchema,
+      validationOptions: { allowUnknown: true, abortEarly: true },
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
---
+++ nest-server/src/configs/validationSchema.ts
@@ -0,0 +1,15 @@
+import Joi from 'joi';
+
+const validationSchema = Joi.object({
+  NODE_ENV: Joi.string().valid('development', 'production').required(),
+
+  DOCKER_DB_NAME: Joi.string().required(),
+  DOCKER_DB_USER: Joi.string().required(),
+  DOCKER_DB_HOST: Joi.string().required(),
+  DOCKER_DB_PASSWORD: Joi.string().required(),
+  DOCKER_DB_PORT: Joi.number().required(),
+
+  TOKEN_SECRET_KEY: Joi.string().required(),
+});
+
+export default validationSchema;
