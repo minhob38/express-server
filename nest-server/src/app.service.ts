ca0994354ec15497c479b6df8e672762ad8b0058
kwon, minho <minhob38@gmail.com>
2022-05-15 01:06:36 +0900

    change root api

--- nest-server/src/app.service.ts
+++ nest-server/src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  /* root path */
+  getRoot(): string {
+    return 'my nest server : )';
   }
 }
