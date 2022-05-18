d909e77945823d490657ba00f86cde43145c600d
kwon, minho <minhob38@gmail.com>
2022-05-18 18:52:59 +0900

    clean up code

--- nest-server/src/modules/maps/maps.module.ts
+++ nest-server/src/modules/maps/maps.module.ts
@@ -4,10 +4,10 @@ import { MapsController } from './maps.controller';
 import { MapsService } from './maps.service';
 import { SeoulSggs } from '../../entities/seoul-ssgs.entity';
 import { MapsRepository } from './maps.repository';
-import { LoggerModule } from 'src/logger/logger.module';
+
 @Module({
   providers: [MapsService, MapsRepository, Logger],
   controllers: [MapsController],
-  imports: [TypeOrmModule.forFeature([SeoulSggs]), LoggerModule],
+  imports: [TypeOrmModule.forFeature([SeoulSggs])],
 })
 export class MapsModule {}
