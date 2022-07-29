35854b073e0d4efddfd18fa9cc0e09a5b1153351
kwon, minho <minhob38@gmail.com>
2022-07-29 12:26:45 +0900

    set path alias

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -11,18 +11,18 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { BullModule } from '@nestjs/bull';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthsModule } from './modules/auths/auths.module';
-import { LoggerMiddleware } from './common/middleware/logger.middleware';
-import { BoardsModule } from './modules/boards/boards.module';
-import { MapsModule } from './modules/maps/maps.module';
-import { PagesModule } from './modules/pages/pages.module';
+import { AuthsModule } from '@modules/auths/auths.module';
+import { LoggerMiddleware } from '@common/middleware/logger.middleware';
+import { BoardsModule } from '@modules/boards/boards.module';
+import { MapsModule } from '@modules/maps/maps.module';
+import { PagesModule } from '@modules/pages/pages.module';
 import { LoggerModule } from './logger/logger.module';
-import { StudyModule } from './modules/study/study.module';
-import { TestModule } from './modules/study/test.module';
-import validationSchema from './configs/validation-schema';
-import databaseConfig from './configs/database.config';
-import tokenConfig from './configs/token.config';
-import { SchedulersModule } from './modules/schedulers/schedulers.module';
+import { StudyModule } from '@modules/study/study.module';
+import { TestModule } from '@modules/study/test.module';
+import validationSchema from '@configs/validation-schema';
+import databaseConfig from '@configs/database.config';
+import tokenConfig from '@configs/token.config';
+import { SchedulersModule } from '@modules/schedulers/schedulers.module';
 
 @Module({
   imports: [
